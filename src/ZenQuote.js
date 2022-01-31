1809c23d5b13e38a2ba02df8ba7b5f05a7a2512f
Claudine McGowan <claudine.mcgowan@gmail.com>
2022-01-31 15:26:13 +0000

    added loader

--- src/ZenQuote.js
+++ src/ZenQuote.js
@@ -1,32 +1,44 @@
 import React, { Component} from 'react';
 import axios from 'axios';
+import "./ZenQuote.css"
 
 class ZenQuote extends Component {
     constructor(props){
         super(props);
-        this.state = {quote:" "}
+        this.state = {quote:"", isLoaded: false}
     }
     
     
     componentDidMount() {
         // load data
-        axios.get('https://api.github.com/zen').then(response => {
+        axios.get("https://api.github.com/zen").then(response => {
             setTimeout(
-                function(){
-                    this.setState({ quote: response.data });
+                function() {
+                    this.setState({ quote: response.data, isLoaded: true });
                 }.bind(this),
-            3000 )
-        })
+                3000 
+            );
+        });
         // set state with data
     }
     render() {
+        if (this.state.isLoaded){
         return (
             <div>
-                <h1>Always remember ....</h1>
-                <p>{this.state.quote}</p>
+                    <h1>Always remember ....</h1>
+                    <p>{this.state.quote}</p>
             </div>
         )
     }
+    else {
+        return (
+            <div>
+                <div className='loader'></div>
+            </div>
+        )
+    }
+
+}
 }
 
 export default ZenQuote;
